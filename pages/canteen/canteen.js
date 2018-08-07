cb4aa553b3f8a541bfcbaacf98f90c17b3581ff7
fuyanan <meihuo_fyn@163.com>
2018-08-07 23:15:45 +0800

    商家主页

--- pages/canteen/canteen.js
+++ pages/canteen/canteen.js
@@ -9,15 +9,21 @@ Page({
     autoplay: app.globalData.scrollPage.autoplay,
     interval: app.globalData.scrollPage.interval,
     duration: app.globalData.scrollPage.duration,
-
-    canteenList:[
-      {id:1,name:'第1食堂',image:'/images/canteen/1.png'},
-      { id: 2, name: '第2食堂', image: '/images/canteen/2.png' },
-      { id: 3, name: '第3食堂', image: '/images/canteen/3.png' },
-      { id: 4, name: '第4食堂', image: '/images/canteen/4.png' },
-      { id: 5, name: '第5食堂', image: '/images/canteen/1.png' },
-      { id: 6, name: '第6食堂', image: '/images/canteen/2.png' }
+    categoryList:[
+      {id:1,name:'快餐便当',image:'/images/canteen/1.png'},
+      { id: 2, name: '汉堡薯条', image: '/images/canteen/2.png' },
+      { id: 3, name: '炸鸡炸串', image: '/images/canteen/3.png' },
+      { id: 4, name: '煲仔烧腊', image: '/images/canteen/4.png' },
+      { id: 5, name: '川菜', image: '/images/canteen/1.png' },
+      { id: 6, name: '粤港菜', image: '/images/canteen/3.png' },
+      { id: 7, name: '麻辣烫', image: '/images/canteen/4.png' },
+      { id: 8, name: '粥面类', image: '/images/canteen/2.png' },
+      { id: 9, name: '小炒', image: '/images/canteen/1.png' },
+      { id: 10, name: '甜品饮品', image: '/images/canteen/4.png' },
+      { id: 11, name: '麻辣香锅', image: '/images/canteen/4.png' }
     ],
+    category:{},
+    catePages : [],
     merchantList:[
       {id:1,name:'广州天河总店',isBrand:1,isShop:1,isSend:1,onTime:1,distance:1000,start:1,shipfee:1,shipfree:1,rest:0,image:'/images/canteen/5.png'},
       {id:2,name:'广州白云总店',isBrand:0,isShop:1,isSend:0,onTime:1,distance:899,start:1,shipfee:1,shipfree:1,rest:0,image:'/images/canteen/5.png'},
@@ -39,7 +45,20 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    console.log(this.data.categoryList.length);
+    let catesLen = this.data.categoryList.length;
+    let page = Math.ceil(catesLen / 8);
+    let category = {};
+    let catePages = [];
+    for (let i = 0; i < page ; i++){
+      catePages.push(i);
+      category[i] = this.data.categoryList.slice(i*8,(i+1)*8);
+    }
+    console.log(category)
+    this.setData({
+      catePages: catePages,
+      category: category
+    })
   },
 
   /**
